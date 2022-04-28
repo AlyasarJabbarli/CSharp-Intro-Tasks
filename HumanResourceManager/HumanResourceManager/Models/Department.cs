d333cc33e1df1ea06f5a9b76f4b3f0580e87ac70
AlyasarJabbarli <alyasarmj@code.edu.az>
2022-04-28 13:51:49 +0400

    Ready

---
+++ HumanResourceManager/HumanResourceManager/Models/Department.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanResourceManager.Models
+{
+    class Department
+    {
+        private string _name;
+        public string Name 
+        {
+            get => _name;
+            set 
+            {
+                while (value.Length<2)
+                {
+                    Console.WriteLine("Departament adi minimum 2 herfden ibaret olmalidir");
+                    value = Console.ReadLine();
+                }
+                _name = value;
+
+            }
+        }
+        private int _workerlimit;
+        public int WorkerLimit 
+        {
+            get => _workerlimit;
+            set 
+            {
+                while (value < 1) 
+                {
+                    Console.WriteLine("Departmanetde maximum var ola bilicek isci sayi minimum 1 ola biler");
+                    int.TryParse(Console.ReadLine() , out value);
+                }
+                _workerlimit = value;
+               
+            }
+        }
+        internal double _salarylimit;
+        public double SalaryLimit 
+        {
+            get => _salarylimit;
+            set
+            {
+                while (value < WorkerLimit*250)
+                {
+                    Console.WriteLine($"Emek Haqqi A.R Qanunvericiliyine uygun olaraq bir isci ucun 250 AZN den az ola bilmez , Ve bu department ucun umumi emek haqqi budce limiti {_workerlimit*250}AZN olmalidir");
+                    double.TryParse(Console.ReadLine() , out value);
+                }
+                _salarylimit = value;
+                Console.WriteLine("Emaliyyat Ugurla Yekunlasdi");
+            }
+        }
+        public Employee[] Employees;
+        public void CalcSalaryAverage() 
+        {
+            double salarysum = 0;
+            for (int i = 0; i < Employees.Length; i++)
+            {
+               salarysum += Employees[i].Salary;
+            }
+            double AverageSalary = salarysum / Employees.Length;
+            Console.WriteLine(AverageSalary);
+        }
+        public Department() 
+        { 
+        }
+        public Department(string name,int workerlimit,double salarylimit) 
+        {
+            Employees = new Employee[0];
+            Name = name.Trim().ToUpper();
+            WorkerLimit = workerlimit;
+            SalaryLimit = salarylimit;
+        }
+        public void AddEmployee(Employee employee) 
+        {
+            if (Employees.Length < WorkerLimit) 
+            {
+                Array.Resize(ref Employees, Employees.Length + 1);
+                Employees[Employees.Length - 1] = employee;
+                Console.WriteLine("Emeliyyat Ugurla Yekunlasdi");
+            }
+            else 
+            {
+                Console.WriteLine("Departamentde yer yoxdur");
+            }
+        }
+        public override string ToString()
+        {
+            return $"Departamentin adi : {_name}" +
+                $"\n Departmanetde maximum var ola bilicek isci sayi : {_workerlimit}" +
+                $"\n Departamentin umumi budce limiti : {_salarylimit}";
+        }
+    }
+}
---
+++ HumanResourceManager/HumanResourceManager/Models/Employee.cs
@@ -0,0 +1,63 @@
+﻿using HumanResourceManager.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HumanResourceManager.Models
+{
+    class Employee : Department 
+    {
+        private static int _count = 1000;
+        public string No;
+        public string FullName { get; set; }
+        private string _position;
+        public string Position 
+        {
+            get => _position;
+            set
+            {
+                while(value.Length < 2)
+                {
+                    Console.WriteLine("Vezife Minimum 2 Herfden ibaret olmalidir");
+                    value = Console.ReadLine();
+                }
+                _position = value;
+            }
+        }
+        public double _salary;
+        public double Salary
+        {
+            get => _salary;
+            set
+            {
+                while (value < 250)
+                {
+                    Console.WriteLine($"Emek Haqqi A.R Qanunvericiliyine uygun olaraq 250 AZN den az ola bilmez");
+                    double.TryParse(Console.ReadLine(), out value);
+                }
+                _salary = value;
+            }
+        }
+        public string DepartmentName { get; set; }
+
+        HumanResourceService humanResourceManager = new HumanResourceService();
+        public Employee(string fullname ,  string position , double salary , string departmentname) 
+        {
+            _count++;
+            FullName = fullname.Trim();
+            Position = position.Trim().ToUpper();
+            Salary = salary;
+            DepartmentName = departmentname.ToUpper();
+            No = $"{(DepartmentName.ToString()[0])}{(DepartmentName.ToString()[1])}{_count}";
+        }
+        public override string ToString()
+        {
+            return $"Ischinin Kodu : {No}" +
+                $"\nIshcininadi adi ve soyadi : {FullName}" +
+                $"\n Iscinin Maasi : {_salary}" +
+                $"\n Iscinin Vezifesi: {_position}" +
+                $"\n Iscinin islediyi Departament : {DepartmentName}";
+        }
+    }
+    
+}
